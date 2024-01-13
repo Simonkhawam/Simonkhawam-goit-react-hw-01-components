d4f2b9c0e12e3f7e23aee97e7ae495e0b8389b9c
Simon Khawam <khawam.simon@me.com>
2024-01-13 22:19:18 +0200

    commit change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import Profile from './Profile';
+import user from '../user/user.json';
+import Statictics from './Statistics';
+import data from '../user-II/data.json';
+import FriendList from './FriendList';
+import friends from '../user-II/friend-list/friends.json';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statictics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
     </div>
   );
 };
