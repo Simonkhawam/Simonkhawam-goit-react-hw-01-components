0b6976a9068d9ed8534c47f37b9c08ffab2d29af
Simon Khawam <khawam.simon@me.com>
2024-01-15 14:56:24 +0200

    commit change ready to send homework

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,17 +1,40 @@
 import style from './css/statistics.module.css';
+
+const getColorForPercentage = percentage => {
+  const colorScale = [
+    'rgb(212, 40, 40)',
+    'yellow',
+    'green',
+    'red',
+    'violet',
+    'turquoise',
+  ];
+
+  const index = Math.floor((percentage / 100) * colorScale.length);
+  return colorScale[index];
+};
+
 const statistics = ({ title, stats }) => {
   return (
     <section className={style.statistics}>
       <h2 className={style.title}>{title}</h2>
       <ul className={style.statList}>
-        {stats.map(item => (
+        {stats.map((item, index) => (
           <li className={style.item} key={item.id}>
             <span className={style.label}>{item.label} </span>
-            <span className={style.percentage}>{item.percentage}%</span>
+            <span
+              className={style.percentage}
+              style={{
+                backgroundColor: getColorForPercentage(item.percentage),
+              }}
+            >
+              {item.percentage}%
+            </span>
           </li>
         ))}
       </ul>
     </section>
   );
 };
+
 export default statistics;
